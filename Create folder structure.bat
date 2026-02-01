df2d58eab88d3e168fe87f1302ed1d6de906cabf
Deepa <Software engineer>
2026-02-01 13:47:57 +0530

    Commit Startup folder structure and Initial files

---
+++ Create folder structure.bat
@@ -0,0 +1,21 @@
+mkdir ai-decision-platform
+cd ai-decision-platform
+git init
+cd..
+mkdir ai-decision-platform\data
+mkdir ai-decision-platform\src
+mkdir ai-decision-platform\src\ingest
+mkdir ai-decision-platform\src\features
+mkdir ai-decision-platform\src\decisions
+mkdir ai-decision-platform\src\actions
+type nul > ai-decision-platform\data\survey.csv
+type nul > ai-decision-platform\src\ingest\load_survey.py
+type nul > ai-decision-platform\src/features/engagement.py
+type nul > ai-decision-platform\src/features/ontology.py
+type nul > ai-decision-platform\src/decisions/knowledge_graph.py
+type nul > ai-decision-platform\src/decisions/rules.py
+type nul > ai-decision-platform\src/decisions/core.py
+type nul > ai-decision-platform\src/actions/actions.py
+type nul > ai-decision-platform\src/app.py
+type nul > ai-decision-platform/README.md
+type nul > ai-decision-platform/requirements.txt
